910b77ccb64e01767254ebea4a19a05b6f6de707
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2024-09-02 18:56:12 +0530

    c

---
+++ Color_Changer/ColorChanger.js
@@ -0,0 +1,28 @@
+console.log(`Shree Krishna`);
+const buttons = document.querySelector(`.button`);
+const body = document.querySelector(`body`);
+console.log(buttons);
+
+// buttons.forEach((button) => {
+//     button.addEventListener(`click`, function(e){
+//         console.log(e);
+//     })
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
