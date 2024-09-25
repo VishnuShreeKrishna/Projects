6d2214a26f2749ee2cb43bc1be55b20c2c93a504
VishnuShreeKrishna <Vishnusharma5615@gmail.com>
2024-09-25 09:00:18 +0530

    commit

--- Color_Changer/ColorChanger.js
+++ Color_Changer/ColorChanger.js
@@ -3,14 +3,14 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 buttons.forEach((button) => {
     button.addEventListener('click', function(e) {
-        console.log(e.target);
+        // console.log(e.target);
         
         const validColors = ['orange', 'white', 'green', 'yellow'];
          
         if (validColors.includes(e.target.id)) {
             // Remove any existing color classes from the body
             validColors.forEach(color => body.classList.remove(color));
-            console.log(body)
+            // console.log(body)
           body.classList.add(e.target.id);
         }
         buttons.forEach((btn) => {
