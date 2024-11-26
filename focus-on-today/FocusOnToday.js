bc76dfc685d0a827f144b3a80069e77365a8b819
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2024-11-26 14:50:53 +0530

    coomit

--- focus-on-today/FocusOnToday.js
+++ focus-on-today/FocusOnToday.js
@@ -17,30 +17,20 @@ checkBoxList.forEach((checkbox) => {
         if(allGoalsAdded){
             checkbox.parentElement.classList.toggle('completed')
             progressValue.style.width = '33.33%'
-            const inputId = checkbox.nextElementSibling.id
-            allGoals[inputId].completed = !allGoals[inputId].completed
-            localStorage.setItem('allGoals', JSON.stringify(allGoals))
         } else{
-           progressBar.classList.add('show-error');
+           progressBar.classList.add('show-error')
         }
     })
 })
 
 inputfields.forEach((input) => {
-    input.value = allGoals[input.id].name
-    if(allGoals[input.id].completed){
-        input.parentElement.classList.add('completed')
-    }
-
     input.addEventListener('focus', () => {
         progressBar.classList.remove('show-error');
     })
-    input.addEventListener('input', (e) => {
-        allGoals[input.id] = {
-            name: input.value,
-            completed: false
-        };
+    input.addEventListener('input', (e) =>{
+        allGoals[input.id] = input.value
         localStorage.setItem('allGoals', JSON.stringify(allGoals))
+        
         
     })
 })
