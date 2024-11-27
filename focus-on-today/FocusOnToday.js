8fe7c897b39650c84c4c9d879ea8b0bc6c6d6d29
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2024-11-27 14:57:09 +0530

    commit

--- focus-on-today/FocusOnToday.js
+++ focus-on-today/FocusOnToday.js
@@ -1,6 +1,7 @@
 const checkBoxList = document.querySelectorAll('.custom-checkbox');
 const inputfields = document.querySelectorAll('.goal-input');
 const errorLabel = document.querySelector('.error-label')
+const progressLabel = document.querySelector('.progress-label')
 const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
 
@@ -15,6 +16,9 @@ const allGoals = JSON.parse(localStorage.getItem('allGoals'))||{}
 let completedGoalsCount = Object.values(allGoals).filter((goal)=>goal.completed).length
 progressValue.style.width = `${completedGoalsCount/3*100}%`
 localStorage.setItem('allGoals', JSON.stringify(allGoals))
+progressLabel.innerText = allQuotes[completedGoalsCount]
+
+
 
 checkBoxList.forEach((checkbox) => {
     checkbox.addEventListener('click', (event) => {
@@ -29,6 +33,7 @@ checkBoxList.forEach((checkbox) => {
             progressValue.style.width = `${completedGoalsCount/3*100}%`
             progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 Completed`
             localStorage.setItem('allGoals', JSON.stringify(allGoals))
+            progressLabel.innerText = allQuotes[completedGoalsCount]
         } else{
            progressBar.classList.add('show-error')
         }
