7fee69bb5f04ea9e085d6bf1e3ec60d348668a08
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2024-11-27 11:01:22 +0530

    commit

--- focus-on-today/FocusOnToday.js
+++ focus-on-today/FocusOnToday.js
@@ -4,11 +4,17 @@ const errorLabel = document.querySelector('.error-label')
 const progressBar = document.querySelector('.progress-bar')
 const progressValue = document.querySelector('.progress-value')
 
+const allQuotes = [
+    `Raise the bar by completing your goals!`,
+    `Well begun is half done!`,
+    `Just a step away, keep going!`,
+    `Whoa! You just completed all the goals, time for chill`
+]
 
 const allGoals = JSON.parse(localStorage.getItem('allGoals'))||{}
 let completedGoalsCount = Object.values(allGoals).filter((goal)=>goal.completed).length
 progressValue.style.width = `${completedGoalsCount/3*100}%`
-
+localStorage.setItem('allGoals', JSON.stringify(allGoals))
 
 checkBoxList.forEach((checkbox) => {
     checkbox.addEventListener('click', (event) => {
@@ -21,6 +27,7 @@ checkBoxList.forEach((checkbox) => {
             allGoals[inputId].completed =! allGoals[inputId].completed
             completedGoalsCount = Object.values(allGoals).filter((goal)=>goal.completed).length
             progressValue.style.width = `${completedGoalsCount/3*100}%`
+            progressValue.firstElementChild.innerText = `${completedGoalsCount}/3 Completed`
             localStorage.setItem('allGoals', JSON.stringify(allGoals))
         } else{
            progressBar.classList.add('show-error')
@@ -33,6 +40,10 @@ inputfields.forEach((input) => {
         progressBar.classList.remove('show-error');
     })
     input.addEventListener('input', (e) =>{
+        if(allGoals[input.id].completed){
+            e.target.value = allGoals[input.id].name
+            return
+        }
         allGoals[input.id] = {
             name: input.value,
             completed: false,
