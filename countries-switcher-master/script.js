d49a220c3fe96852b07e72307ce431449abe28d1
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2025-01-20 11:04:32 +0530

    first commit 20jan2025

---
+++ countries-switcher-master/script.js
@@ -0,0 +1,9 @@
+// fetch('https://restcountries.com/v3.1/all')
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         data.foreach((country)=>{
+//             console.log(country);
+            
+//         })
+//     })
+console.log(`https://restcountries.com/v3.1/all`);
