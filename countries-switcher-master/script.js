c349a62255184850afc98d588cf62fcaf9e4681b
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2025-01-25 14:27:47 +0530

    Friday last commit due

--- countries-switcher-master/script.js
+++ countries-switcher-master/script.js
@@ -3,16 +3,16 @@ fetch('https://restcountries.com/v3.1/all')
     .then((res)=>res.json())
     .then((data)=>{
         data.forEach((country) => {
-            console.log(country);
+            // console.log(country.capital);
             const Countrycardanchor = document.createElement(`a`);
             Countrycardanchor.innerHTML = `
                 <div class="country-card">
-                    <img src="${country.population}" alt="flag">
+                    <img src="${country.flags.svg}" alt="flag">
                     <div class="card-text">
                         <h3 class="card-title">${country.name.common}</h3>
-                        <p><b>Population: </b>81,770,900</p>
-                        <p><b>Region: </b>Europe</p>
-                        <p><b>Capital: </b>Berlin</p>
+                        <p><b>Population: </b>${country.population}</p>
+                        <p><b>Region: </b>${country.region}</p>
+                        <p><b>Capital: </b>${country.capital}</p>
                     </div>
                 </div>
             `
