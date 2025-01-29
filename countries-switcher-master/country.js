3b1e172f3fb15cceda5ca10b57dd77dcb5dfb0a6
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2025-01-29 15:03:57 +0530

    commit

--- countries-switcher-master/country.js
+++ countries-switcher-master/country.js
@@ -1,6 +1,7 @@
 const countryName = new URLSearchParams(location.search).get(`name`);
 const flagImg = document.querySelector('.country-details img')
 const countrynameh1 = document.querySelector('.country-details h1')
+const nativeName = document.querySelector('.native-name')
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
 .then((res)=> res.json())
@@ -8,6 +9,11 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     console.log(data[0])
     flagImg.src = data[0].flags.svg
     countrynameh1.innerText = data[0].name.common
+    if(data[0].name.nativeName){
+        nativeName.innerHTML = Object.values(data[0].name.nativeName)[0].common
+        // console.log();
+        
+    }
 })
 
 
