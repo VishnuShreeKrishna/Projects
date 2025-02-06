f8e78a93bd637221b1995d23f17e625071a08f12
VishnuShreeKrishna <Vishnusharma5615@gmail.com>
2025-02-06 10:22:08 +0530

    commit

--- countries-switcher-master/countryscript.js
+++ countries-switcher-master/countryscript.js
@@ -1,10 +1,28 @@
 const countriesContainer = document.querySelector('.countries-container');
+const filterbyregion = document.querySelector('.filter-by-region')
+const searchinput = document.querySelector(`.search-container input`)
+let allcountriesdata;
+
 fetch('https://restcountries.com/v3.1/all')
     .then((res)=>res.json())
     .then((data)=>{
+        renderCountry(data)
+        allcountriesdata = data
+    })
+//=========================================================================
+filterbyregion.addEventListener('change',(event)=>{
+    // console.log(event.target.value);
+    console.log(filterbyregion.value);
+    fetch(`https://restcountries.com/v3.1/region/${filterbyregion.value}`)
+    .then((res)=>res.json())
+    .then(renderCountry);
+})
+//==========================================================================
+function renderCountry(data){
+        countriesContainer.innerHTML = ''
         data.forEach((country) => {
             // console.log(`${country.name.common}, => ${country.capital} `);
-            console.log(country.borders);
+            // console.log(country.borders);
             const Countrycardanchor = document.createElement(`a`);
             Countrycardanchor.href = `/countries-switcher-master/country.html?name=${country.name.common}`
             const population = new Intl.NumberFormat('en-IN').format(`${country.population}`);
@@ -21,7 +39,35 @@ fetch('https://restcountries.com/v3.1/all')
             `
             countriesContainer.append(Countrycardanchor);
         });
-    });
+}
+//==========================================================================
+
+searchinput.addEventListener(`input`, (eve)=>{
+    // console.log(eve.target.value);
+    // console.log(allcountriesdata);
+    allcountriesdata.filter((country)=>{
+        const filter = country.name.common.toLowerCase().includes(eve.target.value.toLowerCase())
+        
+    })
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //---------------------------------
 // const countriesContainer = document.createElement(`div`);
 // countriesContainer.classList.add(`countries-container`);
